8b60aae740c44aaf5a5a7974f42c2b3693f6589f
Kapelu <ddaniel_calderon@hotmail.com>
2022-10-09 00:59:59 -0300

    upDate 09/10/2022

--- 03 - JavaScript/00 - Básico/01 - Variables/00 - creacionDeclaracion.js
+++
@@ -1,9 +0,0 @@
-// Creación e Inicialización de una variable
-
-var nombre = 'Daniel'
-var edad = 42
-var estudiante= true
-
-console.log(nombre)   //Daniel
-console.log(edad)   //42
-console.log(estudiante)   //true
--- 03 - JavaScript/00 - Básico/05 - Funciones/03 - funcionConNombre.js
+++
@@ -1,2 +0,0 @@
-const suma = function sumar(a,b) {return a + b}
-console.log(suma(15,10))
