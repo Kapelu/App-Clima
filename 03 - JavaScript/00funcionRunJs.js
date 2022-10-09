e9499df741af9681afe03ba4ba7611fcf6fc9153
Kapelu <ddaniel_calderon@hotmail.com>
2022-10-09 11:29:28 -0300

    upDate 09/10/2022

--- 03 - JavaScript/00funcionRunJs.js
+++
@@ -1,6 +0,0 @@
-/*
- * Aquí hay una función que devuelve una cadena:
- */
-const holaMundo = () => '¡Hola Mundo! 🌎'
-// Cuando se llama a la función, el resultado se muestra a la derecha 👉
-holaMundo()
