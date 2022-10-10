fe39db7d04825d757b0ea26b65b4a8ef35841c1a
Kapelu <ddaniel_calderon@hotmail.com>
2022-10-10 02:20:43 -0300

    upDate 010/10/2022

---
+++ 04 - Node/README.md
@@ -0,0 +1,4 @@
+***import-export =>***   [🎥](https://www.youtube.com/watch?v=29iYdru2KUg&list=PL3aEngjGbYhnrRfZKMxzn79qdgPxL7OWM&index=4)
+
+
+
---
+++ 04 - Node/import-export/operaciones.js
@@ -0,0 +1,17 @@
+export const suma = (numA, numB) => {
+	return numA + numB
+}
+
+export const resta = (numA, numB) => {
+	return numA + numB
+}
+
+export const multiplicacion = (numA, numB) => {
+	return numA + numB
+}
+
+//export = {suma, resta, multiplicacion} 
+
+/*
+agregar a operaciones un console.log(module) y en la consola al tipear node archivo.js me va a indicar toda la informacion que se vaya a exportar en el export
+*/
