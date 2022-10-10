7cfa5f7459a4d415ff283158a9ec256e9a8906a7
Kapelu <ddaniel_calderon@hotmail.com>
2022-10-10 02:22:05 -0300

    upDate 010/10/2022

--- 04 - Node/import-export/index.js
+++ 04 - Node/import-export/index.js
@@ -2,6 +2,8 @@ import {suma} from "./operaciones.js"
 import {resta} from './operaciones.js'
 import {multiplicacion} from './operaciones.js'
 
+
+
 console.log(suma(5,9))
 console.log(resta(9,5))
 console.log(multiplicacion(2,4))
