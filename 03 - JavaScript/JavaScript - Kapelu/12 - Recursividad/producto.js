14879a2e06df0900e0035a13fab15df0f93aa773
Kapelu <ddaniel_calderon@hotmail.com>
2022-10-15 16:51:26 -0300

    upDate 15/10/2022

--- 03 - JavaScript/JavaScript - Kapelu/12 - Recursividad/producto.js
+++ 03 - JavaScript/JavaScript - Kapelu/12 - Recursividad/producto.js
@@ -4,19 +4,16 @@ Ejemplos:
 productOfArray([1,2,3]) debería retornar 6
 productOfArray([1,2,3,10]) debería retornar 60
 */
-// ¿multiplicar array?
-var a = [1, 2, 3].map(function(x) { return x * 5; });
-
-a
-
-//var a = [1, 2, 3].map(function(x) x * 5);
-
 
 function productOfArray(arr) {
-	return arr.map(()=>arr * arr -1)
-	//return arr === 1 ? arr :  arr * productOfArray(arr) -1
+	var multiplicar = 1
+	for (var i = 0; i < arr.length; i++) {
+		console.log(arr[i]);
+		multiplicar = multiplicar * arr[i]
+	}
+	return multiplicar
 }
 
-const arr = [1, 2, 3, 10]
+const arr = [1, 2, 3]
 console.log(arr)
 console.log(productOfArray(arr))
