ec4fd11c78d5a89ae4620997639b5a08a5a4466a
Kapelu <ddaniel_calderon@hotmail.com>
2022-10-15 20:59:29 -0300

    upDate 15/10/2022

--- Bootcamp Henry/Henry M1/m1-03-01-nFactorial.js
+++
@@ -1,9 +0,0 @@
-/*
- * nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un    * número natural, su factorial (representado como n!) es el producto de n   * por todos los números naturales menores que él y mayores a 0. Ejemplo:    * 5! = 5 * 4 * 3 * 2 * 1 = 120
- */
-
-function nFactorial(n) {
-	let factorial = n <= 1 ? 1 : n * nFactorial(n - 1)
-	return factorial
-}
-console.log(nFactorial(5))
