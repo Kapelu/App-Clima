1efe416fd5ca7314e1d1d71e944a49d50eeac26d
Kapelu <ddaniel_calderon@hotmail.com>
2022-10-18 12:15:21 -0300

    sas

--- 04 - Node/import-export/index.js
+++ 04 - Node/import-export/index.js
@@ -1,13 +1,16 @@
 // 1 - const operaciones = require('./operaciones.js')
-
 // 1 - console.log(operaciones)
-
 
 /* console.log(suma(5,9))
 console.log(resta(9,5))
 console.log(multiplicacion(2,4))
+
+https://www.youtube.com/watch?v=29iYdru2KUg&list=PL3aEngjGbYhnrRfZKMxzn79qdgPxL7OWM&index=3
+
  */
 
 /*
 agregar a operaciones un console.log(module) y en la consola al tipear node archivo.js me va a indicar toda la informacion que se vaya a exportar en el export
 */
+//*importar libreria externas
+const users = require('jsonwebtoken')
